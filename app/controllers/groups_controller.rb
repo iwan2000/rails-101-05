cb93b8a9c970d0b076bb292735c7eb7cc4601443
李婉 <iwan16406783@gmail.com>
2017-03-07 20:02:42 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! ,only: [new]
+  
   def index
     @groups = Group.all
   end
@@ -17,7 +19,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    
+
     if @group.save
       redirect_to groups_path
     else
